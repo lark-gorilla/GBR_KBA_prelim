1e2bbf7198c1d33d0ac7978e903298ee7d353ef9
lark-gorilla <mark.gr.miller@gmail.com>
2018-07-15 09:42:55 +0100

    FIrst commit. Project setup

---
+++ BRBO_KBA_demo.R
@@ -0,0 +1,7 @@
+## 15/07/2018
+## Preliminary analyses to demonstrate concept of
+## habitat niche modelling of BGR seabird tracking data
+## to identify and inform KBA designation
+
+## Demo using BRBO data from Raine Island and 
+## prediction to Swains population
